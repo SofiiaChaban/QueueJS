1177ab94a1b44ffb5f75b25bb9517616feae4625
Sofiia Chaban <sofachaban11@gmail.com>
2021-04-30 23:06:01 +0300

    first hw commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # QueueJS
+Project that replicates queue behaviour.
+## Start a project
+ Clone this repository and open `index.html` file in your browser to see result.
+ - Max size of queue - 19 items;
+ - You can`t add an empty item to the queue;
---
+++ js/index.js
@@ -0,0 +1,54 @@
+function Queue() {
+    this.data=JSON.parse(localStorage.getItem('data')) || [];
+    this.add = function(val){
+        this.data.push(val);
+    }
+    this.remove = function(){
+        this.data.shift()
+    }
+    this.isEmpty = () => (this.data.length === 0);
+    this.isMaxSize = () => (this.data.length === 19);
+}
+
+let queue = new Queue();
+let addBtn = document.getElementById('addBtn');
+let removeBtn = document.getElementById('removeBtn');
+let queueContainer = document.getElementById('queueContainer');
+addBtn.style.backgroundColor = '#4CAF50';
+addBtn.onclick = function(){
+    let currInput = document.getElementsByTagName('input')[0];
+    if(currInput.value ===''){
+        alert('Invalid input.');
+    }
+    else if(queue.isMaxSize()){
+        alert('Max size of queue is 19');
+    }
+    else{
+        queue.add(currInput.value);
+        console.log(queue.data.length);
+        localStorage.setItem('data',JSON.stringify(queue.data));
+        let newItem = document.createElement('p');
+        newItem.setAttribute('class','queueItem');
+        newItem.innerHTML = currInput.value;
+        queueContainer.prepend(newItem);
+    }
+    currInput.value='';
+}
+removeBtn.style.backgroundColor = '#f44336';
+removeBtn.onclick = function(){
+    if(queue.isEmpty()){
+        alert('Nothing to remove');
+    }
+    else{        
+        queueContainer.removeChild(queueContainer.lastElementChild);
+        queue.remove();
+        localStorage.setItem('data',JSON.stringify(queue.data));
+    }
+}
+
+queue.data.forEach(function(el){
+    let item = document.createElement('p');
+    item.setAttribute('class','queueItem');
+    item.innerHTML = el;
+    queueContainer.prepend(item);
+})
